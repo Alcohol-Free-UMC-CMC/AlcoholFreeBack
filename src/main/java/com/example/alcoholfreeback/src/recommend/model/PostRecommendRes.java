ef1a8190d0f86295cd440a8c1f8508e8519e8d5e
Yeonji-Lim <view7186@naver.com>
2023-06-11 02:26:54 +0900

    [FEAT] : GPT 추천 레시피 저장 기능 추가 및 내용 변경

--- src/main/java/com/example/alcoholfreeback/src/recommend/model/PostRecommendRes.java
+++ src/main/java/com/example/alcoholfreeback/src/recommend/model/PostRecommendRes.java
@@ -1,12 +1,16 @@
 package com.example.alcoholfreeback.src.recommend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
+@AllArgsConstructor
 public class PostRecommendRes {
-    private String content;
+
+    private Long id;
+    private String name;
+    private String description;
 }
